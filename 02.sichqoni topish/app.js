23e4843ac1435ad47dde9bb8b1423360972508fb
nurbek1212 <rayimberganovnurbek6@gmail.com>
2024-12-24 16:51:40 +0500

    3 ta o`yin tayyor

--- 02.sichqoni topish/app.js
+++ 02.sichqoni topish/app.js
@@ -6,7 +6,95 @@ function enter_peper1() {
         peper.src = "img/1.jpg"
         alert(ball)
     } else {
-        peper.scr = "img/2.gif"
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper2() {
+    let peper = document.querySelector(".peper-2")
+    if (variant == 2){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper3() {
+    let peper = document.querySelector(".peper-3")
+    if (variant == 3){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper4() {
+    let peper = document.querySelector(".peper-4")
+    if (variant == 4){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper5() {
+    let peper = document.querySelector(".peper-5")
+    if (variant == 5){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper6() {
+    let peper = document.querySelector(".peper-6")
+    if (variant == 6){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper7() {
+    let peper = document.querySelector(".peper-7")
+    if (variant == 7){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper8() {
+    let peper = document.querySelector(".peper-8")
+    if (variant == 8){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
+        ball++
+    }
+    
+}
+function enter_peper9() {
+    let peper = document.querySelector(".peper-9")
+    if (variant == 9){
+        peper.src = "img/1.jpg"
+        alert(ball)
+    } else {
+        peper.src = "img/2.gif"
         ball++
     }
     
