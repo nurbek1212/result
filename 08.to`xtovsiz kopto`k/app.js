74ae01444d6fda03c0ec82819bb9e32d35e02d4c
nurbek1212 <rayimberganovnurbek6@gmail.com>
2025-01-03 11:17:53 +0500

    9 day

--- 08.to`xtovsiz kopto`k/app.js
+++ 08.to`xtovsiz kopto`k/app.js
@@ -1,36 +1,36 @@
+const ball=document.querySelector('.ball');
+const container=document.querySelector('.container');
 
-const image = document.querySelector(".ball-play");
+let ballSpeedX=4;
+let ballSpeedY=4;
 
-// Tasodifiy yo'nalish va tezlikni yaratish
-function randomMovement() {
-  const containerWidth = document.querySelectorAll(".play-game").offsetWidth;
-  const containerHeight = document.querySelectorAll(".play-game").offsetHeight;
+// function moveBall() {
+//     const ballRect=ball.getBoundingClientRect();
+//     const containerRect=container.getBoundingClientRect();
 
-  // Tasodifiy boshlanish pozitsiyasi
-  const startX = Math.random() * containerWidth;
-  const startY = Math.random() * containerHeight;
-  const endX = Math.random() * containerWidth;
-  const endY = Math.random() * containerHeight;
+//     if (ballRect.right>=containerRect.right || ballRect.left<=containerRect.left) {
+//         ballSpeedX=-ballSpeedX;
+//     }
+//     if(ballRect.bottom>=containerRect.bottom || ballRect.top<=containerRect.top) {
+//         ballSpeedY=-ballSpeedY;
+//     }
 
-  const duration = Math.random() * 5 + 5; // 5 dan 10 sekundgacha bo‘lgan harakat tezligi
-
-  // Tasvirni yangi pozitsiyaga harakatlantirish
-  image.style.left = `${startX}px`;
-  image.style.top = `${startY}px`;
-
-  // Animatsiya qo‘shish
-  image.style.transition = `all ${duration}s linear`;
-
-  // Tasvirni yangi tasodifiy pozitsiyaga ko‘chirish
-  setTimeout(() => {
-    image.style.left = `${endX}px`;
-    image.style.top = `${endY}px`;
-  }, 100);
-
-  // Harakatni qayta boshlash
-  setTimeout(randomMovement, duration * 1000);
+//     ball.style.left=ball.offsetLeft+ballSpeedX+'px';
+//     ball.style.top=ball.offsetTop+ballSpeedY+'px';
+//     requestAnimationFrame(moveBall);
+// }
+// moveBall();
+function moveball(){
+    const ballrect = ball.getBoundingClientRect()
+    const containerrect = container.getBoundingClientRect()
+    if(ballrect.right >= containerrect.right || ballrect.left <= containerrect.left){
+        ballSpeedX = -ballSpeedX
+    }
+    if(ballrect.bottom>=containerrect.bottom || ballrect.top <= containerrect.top){
+        ballSpeedY = -ballSpeedY
+    }
+    ball.style.left = ball.offsetLeft + ballSpeedX + 'px'
+    ball.style.top = ball.offsetTop + ballSpeedY + 'px'
+    requestAnimationFrame(moveball)
 }
-
-// Dastlabki tasodifiy harakatni boshlash
-randomMovement();
-console.log('fcdxrsxd')
+moveball()
